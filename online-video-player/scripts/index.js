478391326a5c20035cf2caa32e41286e00ab5ddc
Siyanda Mayekiso <s.mayekiso@gmail.com>
2023-01-03 17:29:14 -0600

    Change of Names for Indexing
    Change of Names for Indexing

--- online-video-player/scripts/index.js
+++ online-video-player/scripts/index.js
@@ -2,8 +2,8 @@
 
 const video = new Video({
     wrapperID: "video-wrapper",
-    videoSrc: "static/flowing-water-fountain.mp4",
-    posterSrc: "static/flowing-water-fountain-poster.jpg",
+    videoSrc:"static/clear-flowing-water-fountain.mp4/",
+    posterSrc: "static/clear-flowing-water-fountain-poster.jpg",
     absolute: true,
     hideControlsOnPlay: true,
     progressColor: "cream"
